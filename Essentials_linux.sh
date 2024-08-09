48c9aaece0e71c5760e12110de45948bcd8d1d3e
José Francisco <162625758+neptlapelota@users.noreply.github.com>
2024-08-09 14:59:34 -0300

    Add files via upload

---
+++ Essentials_linux.sh
@@ -0,0 +1,35 @@
+"echo" # exibir conteúdo
+
+"whoami" # usuário logado
+
+"Is" # listar arquivos e diretórios
+
+"cd" # entrar em diretório
+
+"cat" # exibir conteúdo de arquivo de texto
+
+"pwd" # exibir path atual
+
+"touch" # cria arquivo
+
+"mkdir" # cria diretório
+
+"cp" # copia arquivos e diretórios
+
+"mv" # move arquivos e diretórios
+
+"rm" # apaga arquivos e diretórios
+
+"file" # verifica o tipo de arquivo
+
+"su" # troca de usuário
+
+"find" # busca por arquivos e diretórios
+
+"grep" # filtro de texto
+
+"ssh" # acessa servidor remoto
+
+#Editores de Textos Linx:
+"vim"
+"nano"
